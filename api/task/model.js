709955ccab05f04aae2f23beed22227005460b01
MarjaCarty <marja.d.carty@gmail.com>
2020-12-11 12:12:56 -0600

    adjustments

--- api/task/model.js
+++ api/task/model.js
@@ -3,13 +3,12 @@ const db = require("../../data/dbConfig");
 
 module.exports = {
   getTasks() {
-    return db("tasks as t")
-      .join("projects as p", "t.project_id", "p.id")
-      .select(
-        "p.name as project_name",
-        "p.description as project_description",
-        "t.description"
-      );
+    return db("tasks as t").join("projects as p", "t.project_id", "p.id");
+    // .select(
+    //   "p.name as project_name",
+    //   "p.description as project_description",
+    //   "t.description"
+    // );
   },
   createTask(task) {
     return db("tasks")
