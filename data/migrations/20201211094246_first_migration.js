244fcd63861c7f68d9be5f292339ed612b1aba61
MarjaCarty <marja.d.carty@gmail.com>
2020-12-11 10:07:04 -0600

    first migration

---
+++ data/migrations/20201211094246_first_migration.js
@@ -0,0 +1,55 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+      tbl.string("project_name", 128).notNullable();
+      tbl.string("project_description");
+      tbl.boolean("project_completed").notNullable().defaultTo(0);
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+      tbl.string("resource_name", 128).notNullable().unique();
+      tbl.string("resource_description");
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+      tbl.string("task_description").notNullable();
+      tbl.string("task_notes");
+      tbl.boolean("task_completed").notNullable().defaultTo(0);
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("projects_resources", (tbl) => {
+      tbl.increments("projects_resources_id");
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("resource_id")
+        .inTable("resources")
+        .onDelete("RESTRICT")
+        .onDelete("RESTRICT");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("projects_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
+};
