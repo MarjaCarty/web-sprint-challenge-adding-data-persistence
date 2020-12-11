59b94c34aae8a699437e9ab559c4fbef56849c36
MarjaCarty <marja.d.carty@gmail.com>
2020-12-11 11:23:10 -0600

    testing

--- data/migrations/20201211094246_first_migration.js
+++ data/migrations/20201211094246_first_migration.js
@@ -1,37 +1,37 @@
 exports.up = function (knex) {
   return knex.schema
     .createTable("projects", (tbl) => {
-      tbl.increments("project_id");
-      tbl.string("project_name", 128).notNullable();
-      tbl.string("project_description");
-      tbl.boolean("project_completed").notNullable().defaultTo(0);
+      tbl.increments();
+      tbl.string("name", 128).notNullable();
+      tbl.string("description");
+      tbl.boolean("completed").notNullable().defaultTo(0);
     })
     .createTable("resources", (tbl) => {
-      tbl.increments("resource_id");
-      tbl.string("resource_name", 128).notNullable().unique();
-      tbl.string("resource_description");
+      tbl.increments();
+      tbl.string("name", 128).notNullable().unique();
+      tbl.string("description");
     })
     .createTable("tasks", (tbl) => {
-      tbl.increments("task_id");
-      tbl.string("task_description").notNullable();
-      tbl.string("task_notes");
-      tbl.boolean("task_completed").notNullable().defaultTo(0);
+      tbl.increments();
+      tbl.string("description").notNullable();
+      tbl.string("notes");
+      tbl.boolean("completed").notNullable().defaultTo(0);
       tbl
         .integer("project_id")
         .unsigned()
         .notNullable()
-        .references("project_id")
+        .references("id")
         .inTable("projects")
         .onDelete("RESTRICT")
         .onUpdate("RESTRICT");
     })
     .createTable("projects_resources", (tbl) => {
-      tbl.increments("projects_resources_id");
+      tbl.increments();
       tbl
         .integer("project_id")
         .unsigned()
         .notNullable()
-        .references("project_id")
+        .references("id")
         .inTable("projects")
         .onDelete("RESTRICT")
         .onUpdate("RESTRICT");
@@ -39,7 +39,7 @@ exports.up = function (knex) {
         .integer("resource_id")
         .unsigned()
         .notNullable()
-        .references("resource_id")
+        .references("id")
         .inTable("resources")
         .onDelete("RESTRICT")
         .onDelete("RESTRICT");
