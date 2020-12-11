c039bef9ef6f2c0fa244ad26a13ba4a33d026248
MarjaCarty <marja.d.carty@gmail.com>
2020-12-11 12:18:22 -0600

    testing tweaks

--- api/task/model.js
+++ api/task/model.js
@@ -5,11 +5,16 @@ module.exports = {
   getTasks() {
     return db("tasks as t")
       .join("projects as p", "t.project_id", "p.id")
-      .select(
-        "p.name as project_name",
-        "p.description as project_description",
-        "t.description"
+      .then((res) =>
+        res.map((item) => {
+          return { ...item, completed: item.completed ? true : false };
+        })
       );
+    // .select(
+    //   "p.name as project_name",
+    //   "p.description as project_description",
+    //   "t.description"
+    // );
   },
   createTask(task) {
     return db("tasks")
