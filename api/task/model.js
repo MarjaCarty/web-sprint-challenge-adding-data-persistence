176700d3cafae59481d4bd726e4371a27aea26c7
MarjaCarty <marja.d.carty@gmail.com>
2020-12-11 11:41:58 -0600

    name changes

--- api/task/model.js
+++ api/task/model.js
@@ -4,8 +4,8 @@ const db = require("../../data/dbConfig");
 module.exports = {
   getTasks() {
     return db("tasks as t")
-      .join("projects as p", "t.project_id", "p.project_id")
-      .select("p.project_name", "p.project_description", "task_description");
+      .join("projects as p", "t.id", "p.id")
+      .select("p.name", "p.description", "t.description");
   },
   createTask(task) {
     return db("tasks")
